d451ab389628ddbe7e82790d34de0c78cc9da15f
Divyansh <54841396+Divyansh4721@users.noreply.github.com>
2021-03-31 02:06:42 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,176 @@
+var bar1=document.getElementById('bar1');
+var bar2=document.getElementById('bar2');
+var ball=document.getElementById('ball');
+var start1=document.getElementById('start1');
+var Count=0;
+alert("Ping-Pong Game By Divyansh");
+document.addEventListener('keypress',function(event){
+  if(event.key=='a')
+  {
+    if(bar1.offsetLeft>125){
+      bar1.style.marginLeft=bar1.offsetLeft-25+'px';
+      bar2.style.marginLeft=bar2.offsetLeft-25+'px';
+    }
+  }
+  if(event.key=='d')
+  {
+    if((bar1.offsetLeft)<window.innerWidth-125){
+      bar1.style.marginLeft=bar1.offsetLeft+25+'px';
+      bar2.style.marginLeft=bar2.offsetLeft+25+'px';
+    }
+  }
+  if(event.key=='Enter')
+  {
+    start1.style.opacity=0;
+    btmlft();
+  }
+});
+
+function reset(){
+  Count=0;
+  start1.style.opacity=100;
+  bar1.style.marginLeft='50%';
+  bar2.style.marginLeft='50%';
+  ball.style.marginLeft='50%';
+  ball.style.marginTop='60px';
+}
+function failed(){
+  localStorage.setItem("high",Math.max(localStorage.getItem("high"),Count));
+  alert("You Failed. Your Score Is : "+Count+". Hightest Score Till Now : "+localStorage.getItem("high"));
+}
+
+
+function getTanFromDegrees(degrees) {
+  return Math. tan(degrees * Math. PI / 180);
+}
+function random(){
+  return Math.floor((Math.random()*30)+30);
+}
+function btmrgt(){
+  var angle=random();
+  var offsetleft=ball.offsetLeft;
+  var minus=0;
+  var br=setInterval(function()
+  {
+    if(ball.offsetTop>=bar1.offsetTop-55){
+      if(bar1.offsetLeft<ball.offsetLeft-25+125 && bar1.offsetLeft+250>ball.offsetLeft-25+125+50){
+        clearInterval(br);
+        Count++;
+        toprgt();
+      }
+    }
+    if((ball.offsetTop+60)>window.innerHeight){
+      failed();
+      clearInterval(br);
+      reset();
+    }
+    else if((ball.offsetLeft-25+70)>window.innerWidth){
+      clearInterval(br);
+      btmlft();
+    }
+    else{
+      var btm=ball.offsetTop;
+      ball.style.marginTop=btm+5+'px';
+      minus=minus+10;
+      var degree=getTanFromDegrees(angle);
+      var left=minus/degree;
+      ball.style.marginLeft=offsetleft+left+'px';
+    }
+  },40);
+}
+function btmlft(){
+  var angle=random();
+  var degree=getTanFromDegrees(angle);
+  var offsetleft=ball.offsetLeft;
+  var minus=0;
+  var br=setInterval(function()
+  {
+    if(ball.offsetTop>=bar1.offsetTop-55){
+      if(bar1.offsetLeft<ball.offsetLeft-25+125 && bar1.offsetLeft+250>ball.offsetLeft-25+125+50){
+        clearInterval(br);
+        Count++;
+        toplft();
+      }
+    }
+    if((ball.offsetTop+60)>window.innerHeight){
+      failed();
+      clearInterval(br);
+      reset();
+    }
+    else if((ball.offsetLeft-25-10)<0){
+      clearInterval(br);
+      btmrgt();
+    }
+    else{
+      var btm=ball.offsetTop;
+      ball.style.marginTop=btm+5+'px';
+      minus=minus+10;
+      var left=minus/degree;
+      ball.style.marginLeft=offsetleft-left+'px';
+    }
+  },40);
+}
+function toplft(){
+  var angle=random();
+  var degree=getTanFromDegrees(angle);
+  var offsetleft=ball.offsetLeft;
+  var minus=0;
+  var br=setInterval(function()
+  {
+    if(ball.offsetTop<=65){
+      if(bar2.offsetLeft<ball.offsetLeft-25+125 && bar2.offsetLeft+250>ball.offsetLeft-25+125+50){
+        clearInterval(br);
+        Count++;
+        btmlft();
+      }
+    }
+    if(ball.offsetTop<13){
+      failed();
+      clearInterval(br);
+      reset();
+    }
+    else if((ball.offsetLeft-25-10)<0){
+      clearInterval(br);
+      toprgt();
+    }
+    else{
+      var btm=ball.offsetTop;
+      ball.style.marginTop=btm-5+'px';
+      minus=minus+10;
+      var left=minus/degree;
+      ball.style.marginLeft=offsetleft-left+'px';
+    }
+  },40);
+}
+function toprgt(){
+  var angle=random();
+  var degree=getTanFromDegrees(angle);
+  var offsetleft=ball.offsetLeft;
+  var minus=0;
+  var br=setInterval(function()
+  {
+    if(ball.offsetTop<=65){
+      if(bar2.offsetLeft<ball.offsetLeft-25+125 && bar2.offsetLeft+250>ball.offsetLeft-25+125+50){
+        clearInterval(br);
+        Count++;
+        btmrgt();
+      }
+    }
+    if(ball.offsetTop<13){
+      failed();
+      clearInterval(br);
+      reset();
+    }
+    else if((ball.offsetLeft-25+70)>window.innerWidth){
+      clearInterval(br);
+      toplft();
+    }
+    else{
+      var btm=ball.offsetTop;
+      ball.style.marginTop=btm-5+'px';
+      minus=minus+10;
+      var left=minus/degree;
+      ball.style.marginLeft=offsetleft+left+'px';
+    }
+  },40);
+}
